cd8bd7581681e213faf810bdcebb84b594cee56c
Pjotr Lengkeek <p1.lengkeek@gmail.com>
2017-10-11 21:09:01 +0200

    Working EGreedy

--- hopper.py
+++ hopper.py
@@ -22,28 +22,47 @@ class Q_learning:
     def update_q(self, s, a, r, s_next, alpha, gamma):
         max_q = 0
         for i in range(7):
-            if self.get_q(s, i > max_q):
+            if self.get_q(s_next, i) > max_q:
                 max_q = self.get_q(s_next, i)
 
         new_q = self.get_q(s, a) + alpha * (r + gamma - max_q - self.get_q(s, a))
         self.set_q(s, a, new_q)
 
-    def get_random_action(self, a):
-        return random.randint(0, len(a))
+    def get_action(self, index):
+        if index == 0:
+            return np.zeros(3)
+        elif index == 1:
+            return np.array([0.1, 0, 0])
+        elif index == 2:
+            return np.array([-0.1, 0, 0])
+        elif index == 3:
+            return np.array([0, 0.1, 0])
+        elif index == 4:
+            return np.array([0, -0.1, 0])
+        elif index == 5:
+            return np.array([0, 0, 0.1])
+        elif index == 6:
+            return np.array([0, 0, -0.1])
 
-    def get_best_action(self, a):
-        return np.argmax(a)
+    def get_random_action(self, q_values):
+        # a = [nothing, +, -, +, -, +, -]
+        index = random.randint(0, len(q_values))
+        return self.get_action(index)
 
-    def get_EGreedy_action(self, a, epsilon):
-        random_float = random.random()
-        action = np.zeros(7)
-        if random_float < epsilon:
-            action[self.get_random_action(a)] = 0.1
-            return action
+    def get_best_action(self, q_values):
+        same_q = len(np.where(q_values == max(q_values))[0])
+        if same_q >= 2:
+            index = random.randint(0, same_q-1)
         else:
-            action[self.get_best_action(a)] = 0.1
-            return action
+            index = np.argmax(q_values)
+        return self.get_action(index)
 
+    def get_EGreedy_action(self, q_values, epsilon):
+        random_float = random.random()
+        if random_float < epsilon:
+            return self.get_random_action(q_values)
+        else:
+            return self.get_best_action(q_values)
 
 class Hopper:
     def __init__(self):
@@ -79,14 +98,17 @@ class Simulation:
         t = 0
         while True:
 
+
             old_state = self.hopper.get_state()
-            action = self.Q.get_EGreedy_action(old_state, self.epsilon)
-            print(action)
-            new_state = self.hopper.do_action(action)
-            self.Q.update_q(old_state, action, self.get_r(), new_state, self.learning_rate, self.discount_value)
+            action = self.Q.get_best_action(self.Q.get_q(self.hopper.get_state(), [range(7)]))
+            new_state = self.hopper.do_action(action*10)
+            print(new_state)
+            #self.Q.update_q(old_state, action, self.get_r(), new_state, self.learning_rate, self.discount_value)
 
             self.hopper.sim.step()
             t += 0.002
+
+            #print(self.Q.get_q(self.hopper.get_state(), [range(7)]))
 
             self.viewer.render()
 
