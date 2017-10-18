c8b83ab1e1e5b2ff5ac3493036efe8bc69a45bd7
Pjotr Lengkeek <p1.lengkeek@gmail.com>
2017-10-18 21:37:13 +0200

    Added dqn.py as reference solution
    Both codes should be the same yet they dont perform the same so ther must be a mistake in the inverted_pendulum.

--- inverted_pendulum.py
+++ inverted_pendulum.py
@@ -2,142 +2,150 @@ import mujoco_py as mj
 import numpy as np
 import random
 from math import degrees, radians
-import tensorflow as tf
-np.set_printoptions(suppress=True)
-from neural_network import NeuralNetwork
-
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+import gym
 
 class Brain:
-    def __init__(self, layout, functions, memory_size):
-        self.memory_size = memory_size
-        self.replay_memory = np.zeros(memory_size)
-        self.neural_net = NeuralNetwork(layout, functions)
-        self.memories_saved = 0
+    def __init__(self, learning_rate, gamma, epsilon, epsilon_min, epsilon_decay):
+        self.replay_memory = deque(maxlen=2000)
+        self.learning_rate = learning_rate
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.epsilon_min = epsilon_min
+        self.epsilon_decay = epsilon_decay
+        self.net = self._build_model()
+
+    def _build_model(self):
+        # Neural Net for Deep-Q learning Model
+        model = Sequential()
+        model.add(Dense(24, input_dim=4, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(2, activation='linear'))
+        model.compile(loss='mse',
+                      optimizer=Adam(lr=self.learning_rate))
+        return model
 
     # memory = [state, action, reward, new_state]
     def add_memory(self, memory):
-        if self.memories_saved < self.memory_size[0]:
-            self.replay_memory[self.memories_saved] = memory
-            self.memories_saved += 1
-        else:
-            self.replay_memory = np.roll(self.replay_memory, -1, axis=0)
-            self.replay_memory[-1] = memory
-            self.memories_saved += 1
+        self.replay_memory.append(memory)
 
-    def train(self, train_x, train_y, epochs, learning_rate):
-        self.neural_net.train(train_x, train_y, epochs, learning_rate)
+    def replay(self, batch_size):
+        mini_batch = random.sample(self.replay_memory, batch_size)
+        training_x = np.zeros((1, 4))
+        training_y = np.zeros((1, 2))
+        for state, action, reward, next_state, done in mini_batch:
+            target = reward
+            if not done:
+                target = (reward + self.gamma *
+                          np.amax(self.net.predict(next_state)[0]))
+            target_f = self.net.predict(state)
+            target_f[0][action] = target
 
-    def predict(self, x):
-        return self.neural_net.forward(x/1.57)*10.
+            training_x = np.vstack((training_x, state))
+            training_y = np.vstack((training_y, target_f))
+
+        np.delete(training_x, 0, axis=0)
+        np.delete(training_y, 0, axis=0)
+
+        self.net.fit(training_x, training_y, epochs=10, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
 
 
 class InvertedPendulum(Brain):
-    def __init__(self, layout, functions, memory_size):
-        Brain.__init__(self, layout, functions, memory_size)
+    def __init__(self, learning_rate, gamma, epsilon, epsilon_min, epsilon_decay):
+        Brain.__init__(self, learning_rate, gamma, epsilon, epsilon_min, epsilon_decay)
         self.model = mj.load_model_from_path('xml/inverted_pendulum.xml')
         self.sim = mj.MjSim(self.model)
-        self.initial_state = self.sim.get_state()
-
-    def get_state(self):
-        return self.sim.get_state().qpos[1]
 
     def get_action(self):
-        return np.array([[0], [-1], [1]])
+        return np.array([[-1], [1]])
 
     def do_action(self, a):
         self.sim.data.ctrl[0] = a
-        return self.get_state()
 
-    def reset(self):
-        self.sim.set_state(self.initial_state)
 
-    def is_alive(self):
-        if degrees(self.sim.get_state().qpos[1]) > 60 or degrees(self.sim.get_state().qpos[1]) < -60:
+class Environment:
+    def __init__(self):
+        self.inverted_pendulum = InvertedPendulum(learning_rate=0.001, gamma=0.95, epsilon=1, epsilon_min=0.01,
+                                                  epsilon_decay=0.995)
+        self.viewer = None
+        self.sim = self.inverted_pendulum.sim
+        self.initial_state = self.sim.get_state()
+
+    def is_done(self):
+        angle = degrees(self.sim.get_state().qpos[1])
+        if 20. > angle > -20.:
             return False
         else:
             return True
 
+    def get_reward(self):
+        return 1
 
-class Simulation:
+
+    def step(self, action):
+        self.inverted_pendulum.do_action(action)
+        self.sim.step()
+        state = np.array([self.sim.get_state().qpos.tolist() + self.sim.get_state().qvel.tolist()])
+        reward = self.get_reward()
+        return state, reward, self.is_done()
+
+    def render(self):
+        if not self.viewer:
+            self.viewer = mj.MjViewer(self.inverted_pendulum.sim)
+        self.viewer.render()
+
+    def reset(self):
+        self.sim.set_state(self.initial_state)
+        return np.array([self.initial_state.qpos.tolist() + self.initial_state.qvel.tolist()])
+
+
+class Simulation(Environment):
     def __init__(self):
-        self.inverted_pendulum = InvertedPendulum(layout=[1, 10, 3], functions=['sigmoid', 'sigmoid'], memory_size=(300, 4))
-        self.viewer = mj.MjViewer(self.inverted_pendulum.sim)
-        self.learning_rate = 0.01
-        self.discount_value = 0.9
-        self.epsilon = 1
-
-    def get_reward(self):
-        angle = degrees(self.inverted_pendulum.sim.get_state().qpos[1])
-        if angle < 5 and angle > -5:
-            reward = 10
-        elif (angle > 5 and angle < 45) and (angle < -5 and angle > -45):
-            reward = -1
-        else:
-            reward = -10
-        return reward
-    '''''
-
-    def get_reward(self):
-        angle = self.inverted_pendulum.sim.get_state().qpos[1]
-        reward = min(50., 1 / angle) / 50
-        return reward
-    '''''
+        self.env = Environment.__init__(self)
+        self.epochs = 1000
+        self.batch_size = 32
+        self.env = gym.make('CartPole-v1')
 
     def run(self):
-        frames = -1
+        for e in range(self.epochs):
+            state = self.reset()
+            state = self.env.reset()
+            state = np.reshape(state, [1, 4])
+            for time in range(500):
 
-        while True:
-            frames += 1
-            # Lower the actions per second to about 60
+                #self.render()
 
-            past_state = self.inverted_pendulum.get_state()
-            q_values = self.inverted_pendulum.predict(past_state)
+                q_values = self.inverted_pendulum.net.predict(state)
+                if random.random() < self.inverted_pendulum.epsilon:
+                    index = random.randint(0, len(q_values[0]) - 1)
+                else:
+                    index = np.argmax(q_values)
+                action = self.inverted_pendulum.get_action()[index]
 
-            if random.random() < self.epsilon:
-                index = random.randint(0, len(q_values[0]) - 1)
-            else:
-                index = np.argmax(q_values)
-            action = self.inverted_pendulum.get_action()[index]
+                new_state, reward, done, _ = self.env.step(index)
+                new_state = np.reshape(new_state, [1, 4])
+                #new_state, reward, done = self.step(action)
+                #reward = self.get_reward()
+                reward = reward if not done else -10
 
-            self.inverted_pendulum.do_action(action)
-            self.inverted_pendulum.sim.step()
+                memory = (state, action, reward, new_state, done)
+                self.inverted_pendulum.add_memory(memory)
 
-            reward = self.get_reward()
-            new_state = self.inverted_pendulum.get_state()
+                state = new_state
+                if done:
+                    print("episode: {}/{}, score: {}, e: {}".format(e, self.epochs, time,
+                                                                    self.inverted_pendulum.epsilon))
+                    break
 
-            memory = [past_state] + [reward] + [index] + [new_state]
-            self.inverted_pendulum.add_memory(memory)
-
-            if frames > self.inverted_pendulum.memory_size[0]:
-                sample_size = 100
-                sample = self.inverted_pendulum.replay_memory[np.random.randint(self.inverted_pendulum.memory_size[0], size=sample_size)]
-                if self.inverted_pendulum.is_alive():
-                    train_x = sample[:, 0:1]
-                    train_y = self.inverted_pendulum.predict(train_x)
-                    max_q_prime = self.inverted_pendulum.predict(sample[:, 3:])
-                    for i in range(len(train_y)):
-                        train_y[i][int(sample[:, 2][i])] = sample[:, 1][i] + self.discount_value * np.max(max_q_prime[i])
-
-            if frames % 300 == 0 and frames > self.inverted_pendulum.memory_size[0]:
-
-                #
-                # Looks like it is reverse learning meaning killing itself as fast as possible
-                #
-
-                print('Training')
-                self.inverted_pendulum.train(train_x/1.57, train_y/10., 100000, self.learning_rate)
-                print(train_x/1.57)
-                print(self.inverted_pendulum.predict(train_x))
-                print(self.inverted_pendulum.neural_net.error[0]/self.inverted_pendulum.neural_net.error[-1])
-                self.epsilon *= 0.9
-
-            else:
-
-                if not self.inverted_pendulum.is_alive():
-                    self.inverted_pendulum.reset()
-
-                self.inverted_pendulum.sim.step()
-                self.viewer.render()
+            if len(self.inverted_pendulum.replay_memory) > self.batch_size:
+                self.inverted_pendulum.replay(self.batch_size)
+                # if e % 10 == 0:
+                #     agent.save("./save/cartpole-dqn.h5")
 
 simulation = Simulation()
 simulation.run()
--- neural_net_tf.py
+++ neural_net_tf.py
@@ -5,27 +5,23 @@ class NeuralNet:
     def __init__(self, learning_rate, layout):
         self.learning_rate = learning_rate
         self.layout = layout
+        self.net = self.model()
 
     def model(self):
         net = tflearn.layers.core.input_data(shape=[None, self.layout[0]], name='input')
 
         net = tflearn.layers.core.fully_connected(net, self.layout[1], activation='sigmoid')
 
-        net = tflearn.layers.core.fully_connected(net, self.layout[2], activation='softmax')
-        net = tflearn.layers.estimator.regression(net, optimizer='sgd', learning_rate=self.learning_rate,
+        net = tflearn.layers.core.fully_connected(net, self.layout[2], activation='sigmoid')
+        net = tflearn.layers.estimator.regression(net, optimizer='adam', learning_rate=self.learning_rate,
                                                   loss='categorical_crossentropy', name='targets')
         model = tflearn.DNN(net, tensorboard_verbose=0)
 
         return model
 
-    def train(self, train_x, train_y, epochs,model=False):
-        if not model:
-            model = self.model()
+    def train(self, train_x, train_y, epochs):
+        self.net.fit({'input': train_x}, {'targets': train_y}, n_epoch=epochs, show_metric=False)
+        return self.net
 
-        model.fit({'input': train_x}, {'targets': train_y}, n_epoch=epochs, show_metric=True)
-        return model
-
-    def predict(self, x, model=False):
-        if not model:
-            model = self.model()
-        return model.predict(x)
+    def predict(self, x):
+        return self.net.predict(x)
