e87c57cafaf77c3b68b4b97908f6dc73b44ba9a8
Pjotr Lengkeek <p1.lengkeek@gmail.com>
2017-10-18 11:40:53 +0200

    Different reward funcion and normalized inputs for neural_net

--- inverted_pendulum.py
+++ inverted_pendulum.py
@@ -2,6 +2,8 @@ import mujoco_py as mj
 import numpy as np
 import random
 from math import degrees, radians
+import tensorflow as tf
+np.set_printoptions(suppress=True)
 from neural_network import NeuralNetwork
 
 
@@ -26,7 +28,7 @@ class Brain:
         self.neural_net.train(train_x, train_y, epochs, learning_rate)
 
     def predict(self, x):
-        return self.neural_net.forward(x)
+        return self.neural_net.forward(x/1.57)*10.
 
 
 class InvertedPendulum(Brain):
@@ -58,64 +60,79 @@ class InvertedPendulum(Brain):
 
 class Simulation:
     def __init__(self):
-        self.inverted_pendulum = InvertedPendulum(layout=[1, 5, 3], functions=['sigmoid', 'sigmoid'], memory_size=(100, 4))
+        self.inverted_pendulum = InvertedPendulum(layout=[1, 10, 3], functions=['sigmoid', 'sigmoid'], memory_size=(300, 4))
         self.viewer = mj.MjViewer(self.inverted_pendulum.sim)
-        self.learning_rate = 0.5
+        self.learning_rate = 0.01
         self.discount_value = 0.9
-        self.epsilon = 0.3
+        self.epsilon = 1
+
+    def get_reward(self):
+        angle = degrees(self.inverted_pendulum.sim.get_state().qpos[1])
+        if angle < 5 and angle > -5:
+            reward = 10
+        elif (angle > 5 and angle < 45) and (angle < -5 and angle > -45):
+            reward = -1
+        else:
+            reward = -10
+        return reward
+    '''''
 
     def get_reward(self):
         angle = self.inverted_pendulum.sim.get_state().qpos[1]
-        reward = min(50., 1/angle) / 50
+        reward = min(50., 1 / angle) / 50
         return reward
+    '''''
 
     def run(self):
         frames = -1
+
         while True:
             frames += 1
-
             # Lower the actions per second to about 60
-            if frames % 8 == 0:
-                past_state = self.inverted_pendulum.get_state()
-                q_values = self.inverted_pendulum.predict(past_state)
 
-                if random.random() < self.epsilon:
-                    index = random.randint(0, len(q_values) - 1)
-                else:
-                    index = np.argmax(q_values)
-                action = self.inverted_pendulum.get_action()[index]
+            past_state = self.inverted_pendulum.get_state()
+            q_values = self.inverted_pendulum.predict(past_state)
 
-                self.inverted_pendulum.do_action(action)
-                self.inverted_pendulum.sim.step()
+            if random.random() < self.epsilon:
+                index = random.randint(0, len(q_values[0]) - 1)
+            else:
+                index = np.argmax(q_values)
+            action = self.inverted_pendulum.get_action()[index]
 
-                reward = self.get_reward()
-                new_state = self.inverted_pendulum.get_state()
+            self.inverted_pendulum.do_action(action)
+            self.inverted_pendulum.sim.step()
 
-                memory = [past_state] + [reward] + [index] + [new_state]
-                self.inverted_pendulum.add_memory(memory)
+            reward = self.get_reward()
+            new_state = self.inverted_pendulum.get_state()
 
-                if frames > self.inverted_pendulum.memory_size[0]*8:
-                    sample_size = 25
-                    sample = self.inverted_pendulum.replay_memory[np.random.randint(self.inverted_pendulum.memory_size[0], size=sample_size)]
-                    if self.inverted_pendulum.is_alive():
-                        train_x = sample[:, 0:1]
-                        train_y = self.inverted_pendulum.predict(train_x)
-                        max_q_prime = self.inverted_pendulum.predict(sample[:, 3:])
-                        for i in range(len(train_y)):
-                            train_y[i][int(sample[:, 2][i])] = sample[:, 2][i] + self.discount_value * np.max(max_q_prime[i])
+            memory = [past_state] + [reward] + [index] + [new_state]
+            self.inverted_pendulum.add_memory(memory)
 
-                if frames % 800 == 0 and frames > self.inverted_pendulum.memory_size[0]*8:
+            if frames > self.inverted_pendulum.memory_size[0]:
+                sample_size = 100
+                sample = self.inverted_pendulum.replay_memory[np.random.randint(self.inverted_pendulum.memory_size[0], size=sample_size)]
+                if self.inverted_pendulum.is_alive():
+                    train_x = sample[:, 0:1]
+                    train_y = self.inverted_pendulum.predict(train_x)
+                    max_q_prime = self.inverted_pendulum.predict(sample[:, 3:])
+                    for i in range(len(train_y)):
+                        train_y[i][int(sample[:, 2][i])] = sample[:, 1][i] + self.discount_value * np.max(max_q_prime[i])
 
-                    #
-                    # Looks like it is reverse learning meaning killing itself as fast as possible
-                    #
+            if frames % 300 == 0 and frames > self.inverted_pendulum.memory_size[0]:
 
-                    print('Training')
-                    self.inverted_pendulum.train(train_x, train_y, 100, self.learning_rate)
-                    print(self.inverted_pendulum.neural_net.error[-1])
+                #
+                # Looks like it is reverse learning meaning killing itself as fast as possible
+                #
+
+                print('Training')
+                self.inverted_pendulum.train(train_x/1.57, train_y/10., 100000, self.learning_rate)
+                print(train_x/1.57)
+                print(self.inverted_pendulum.predict(train_x))
+                print(self.inverted_pendulum.neural_net.error[0]/self.inverted_pendulum.neural_net.error[-1])
+                self.epsilon *= 0.9
 
             else:
-            
+
                 if not self.inverted_pendulum.is_alive():
                     self.inverted_pendulum.reset()
 
