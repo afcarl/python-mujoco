b7eef5acb1dd8304131086df8c1c5f012886a4ce
Pjotr Lengkeek <p1.lengkeek@gmail.com>
2017-10-21 17:26:22 +0200

    Added plotter

---
+++ plotter.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+files = os.listdir("./logs")
+data = np.genfromtxt("./logs/" + files[-1], comments="#")
+
+N = 50
+moving_mean = np.convolve(data, np.ones((N,))/N, mode='valid')
+plt.scatter(range(len(data)), data)
+plt.plot(range(int(N/2 - 1), len(data) - int(N/2)), moving_mean, 'r')
+plt.show()
